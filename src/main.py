4051dd156c16c7f4056fd6362b1d52f17af95e87
jordanOsorioB <jo.osoriob@duocuc.cl>
2025-05-10 12:37:36 -0400

    error de merge

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from fastapi.templating import Jinja2Templates
 import requests
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI() 
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
